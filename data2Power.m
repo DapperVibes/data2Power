27557c8b5a57e84e61d39a03e106f8a4c8635d31
DapperVibes <31927305+DapperVibes@users.noreply.github.com>
2017-12-20 15:44:31 +0000

    First Draft

---
+++ data2Power.m
@@ -0,0 +1,38 @@
+function [appPower,disPower,rtvPower] = data2Power(voltage,current,dt)
+%data2Power - This code takes waveforms for voltage and current and uses
+%them to calculate the apparent power, dissipated power, and reactive power
+%
+% Syntax:  [dissipatedPower,reactivePower] = data2Power(voltage,current,dt)
+%
+% Inputs:
+%    voltage - Voltage time series [V]
+%    current - Current time series [A]
+%    dt - Time step [s]
+%
+% Outputs:
+%    appPower - Apparent Power
+%    disPower - Dissipated Power
+%    rtvPower - Description
+%
+% Example: 
+%    Line 1 of example
+%    Line 2 of example
+%    Line 3 of example
+%
+% Other m-files required: none
+% Subfunctions: none
+% MAT-files required: none
+%
+% See also:
+
+% Author: Craig Dolder
+% Work address
+% email: C.N.Dolder@soton.ac.uk
+% Dec 2017; Last revision: 20-Dec-2017
+
+%------------- BEGIN CODE --------------
+
+
+
+%------------- END OF CODE --------------
+
---
+++ test_data2Power.m
@@ -0,0 +1,16 @@
+%% Code for testing data2Power function
+%
+% Objective 1, similate several waveforms typical of PZT transducers
+%
+% Objective 2, calculate the theoretical low amplitude apparent,
+% dissipated, and reactive power.
+%
+% Objective 3, code an fft solution to calculating the powers
+%
+% Objective 4, test against theoretical values
+%
+%% Objective 1, similate several waveforms typical of PZT transducers
+%
+% Load experimental impedance
+
+why
